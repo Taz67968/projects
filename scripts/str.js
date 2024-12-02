08ddd1aa6a384c9f562c01d6b10456afa4033c34
Tazoh Cliff <tazohcliff@gmail.com>
2024-12-02 16:59:41 +0100

    work

---
+++ scripts/str.js
@@ -0,0 +1,22 @@
+  //Declare sentence
+  // create a function
+  //split the sentence
+  //intialise a variable that tracks the words individually
+  //loop the individual words to iterate through the words
+  //return longestword
+  //console.log the function
+const str = ('the', 'longest', 'word', 'in', 'my', 'sentence')
+function longestWord(str) {
+    const words = str.split('')
+    let longest = 0
+    let long
+
+    for (let index = 0; index < words.length; index++) {
+        if (words[index].length > longest) {
+            longest = words[index].length
+            long = words[index]
+        }
+    }
+    return long
+}
+console.log(longestWord(str))
